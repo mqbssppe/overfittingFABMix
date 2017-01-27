3c1182a6cf9a566e5c5062f961762fb445c50ea5
mqbssppe <panagiotis.papastamoulis@manchester.ac.uk>
2017-01-27 09:37:45 +0000

    update README

--- simulations/example.R
+++ simulations/example.R
@@ -1,7 +1,7 @@
 # ****************************************************************#
 # (a) Simulate a dataset along the lines of the paper.           #
-#	It follows the scenario used in subsection:             #
-#	``Estimating the number of clusters''                  #
+#       It follows the scenario used in subsection:             #
+#       ``Estimating the number of clusters''                  #
 # (b) Run fabMix                                               #
 # (c) Run mclust and flexmix                                    #
 # (d) Compare estimated clusters against the true classification #
