79c1329c51161f89c70da1e9fe07342e29fa2148
mqbssppe <papapast@yahoo.gr>
2018-11-23 09:42:36 +0200

    add simData2

--- fabMixPackage/version_4.4/fabMix.Rcheck/fabMix-Ex.R
+++ fabMixPackage/version_4.4/fabMix.Rcheck/fabMix-Ex.R
@@ -1065,6 +1065,33 @@ summary(syntheticDataset)
 
 
 cleanEx()
+nameEx("simData2")
+### * simData2
+
+flush(stderr()); flush(stdout())
+
+### Name: simData2
+### Title: Synthetic data generator 2
+### Aliases: simData2
+
+### ** Examples
+
+library('fabMix')
+
+n = 8                # sample size
+p = 5                # number of variables
+q = 2                # number of factors
+K = 2                # true number of clusters
+
+sINV_diag = 1/((1:p))    # diagonal of inverse variance of errors
+set.seed(100)
+syntheticDataset <- simData2(K.true = K, n = n, q = q, p = p, 
+                        sINV_values = sINV_diag)
+summary(syntheticDataset)
+
+
+
+cleanEx()
 nameEx("update_OmegaINV")
 ### * update_OmegaINV
 
--- fabMixPackage/version_4.4/fabMix/R/fabMix.R
+++ fabMixPackage/version_4.4/fabMix/R/fabMix.R
@@ -6112,6 +6112,131 @@ simData <- function(sameSigma = TRUE, sameLambda = FALSE, p, q, K.true, n, loadi
 	return(results)
 }
 
+
+
+
+simData2 <- function(sameSigma = TRUE, p, q, K.true, n, loading_means, loading_sd, sINV_values){
+	if(missing(p)){p = 40}
+	if(missing(q)){q = 4}
+	if(missing(K.true)){K.true = 6}
+	if(missing(n)){n = 200}
+	if(missing(sINV_values)){
+		if(sameSigma){ 
+			sINV_values = rgamma(p, shape = 1, rate = 1) 
+		}else{
+			sINV_values = matrix(rgamma(K.true*p, shape = 1, rate = 1), nrow = K.true, ncol = p )
+		}
+	}
+	if( missing(loading_means) ){ loading_means <- c(-30,-20,-10,10, 20, 30) }
+	if( missing(loading_sd) ){ loading_sd <- rep(2, length(loading_means)) }
+
+	if ( length(loading_means) !=  length(loading_sd) ){
+		stop("`loading_means` and `loading_sd` should have same length.")
+	}
+	cat(paste0("Simulation parameters:"),'\n')
+	if(q >= p){stop("q should not be greater than p")}
+	cat(paste0("   n = ", n),'\n')
+	cat(paste0("   p = ", p),'\n')
+	cat(paste0("   q = ", q),'\n')
+	cat(paste0("   K = ", K.true),'\n')
+	w.true <- myDirichlet(rep(10,K.true))
+	Lambda.true <- array(data = rnorm(K.true*p*q, mean = 0, sd = 1), dim = c(K.true,p,q))
+	mu.true <- array(data = 0, dim = c(K.true,p))
+	for(k in 1:K.true){
+		if(runif(1) < 0.5){
+			pickAnIndex <- sample(length(loading_means), 1)
+			u <- runif(1)
+			subROW <- floor(p/q)
+			mySeq <- seq(from = 1, to = loading_means[pickAnIndex], length = p)
+			mySign2 = 1
+			for(j in 1:q){
+				if(j%%2 == 0) { myIndex = p:1}else{myIndex = 1:p}
+				meanCOL <- mySeq
+				mySign = 1*mySign2
+				for(fa in 1:q){
+					meanCOL[ (fa-1)*subROW + 1:subROW] <- mySign*meanCOL[(fa-1)*subROW + 1:subROW]
+					mySign = -1*mySign
+				}
+				Lambda.true[k, , j] <- rnorm(p, mean = meanCOL[myIndex], sd = loading_sd[pickAnIndex] )
+				mySign2 = -1*mySign2
+			}
+		}else{
+			u <- runif(1)
+			subROW <- floor(p/q)
+			for (j in 1:q) {
+			    meanCOL <- rep(0, p)
+			    pickAnIndex <- sample(length(loading_means), 1)
+			    meanCOL[(j - 1) * subROW + 1:subROW] <- loading_means[pickAnIndex]
+				Lambda.true[k, , j] <- rnorm(p, mean = meanCOL,  sd = loading_sd[pickAnIndex])
+				if (j > 1) {
+					Lambda.true[k, 1:(j - 1), j] <- rep(0, j - 1)
+				}
+			}
+
+		}
+		u <- runif(1)
+		if(u < 1/3){
+			mu.true[k, ] <- 20*sin(seq(0,k*pi, length = p))
+		}else{
+			if(u < 2/3){
+				mu.true[k, ] <- 20*cos(seq(0,k*pi, length = p))
+			}else{
+				mu.true[k, ] <- 40*(sin(seq(0,k*pi, length = p)))^2 - 40*(cos(seq(0,k*pi, length = p)))^2
+			}
+		}
+	}
+
+
+	if(sameSigma == TRUE){
+		SigmaINV.true <- array(data = 0, dim = c(p,p))
+		diag(SigmaINV.true) <- sINV_values
+		Sigma.true <- SigmaINV.true
+		diag(Sigma.true) <- 1/diag(SigmaINV.true)
+
+	}else{
+		SigmaINV.true <- array(data = 0, dim = c(K.true, p,p))
+		Sigma.true <- SigmaINV.true
+		for(k in 1:K.true){
+			diag(SigmaINV.true[k,,]) <- sINV_values[k,]
+			diag(Sigma.true[k,,]) <- 1/diag(SigmaINV.true[k,,])
+		}
+	}
+	z.true <- sample(K.true,n,replace = TRUE, prob = w.true)
+	y.true <- array(data = 0, dim = c(n,q))
+	x_data <- array(data = 0, dim = c(n,p))
+	ly <- q
+	for(i in 1:n){
+		y.true[i,] <- rnorm(ly,mean = 0,sd = 1)
+		j <- z.true[i]
+		if(q == 1){
+			x_mean <- mu.true[j,] + Lambda.true[j, , ] %*% array(y.true[i, ], dim = c(q,1))
+		}else{
+			x_mean <- mu.true[j,] + Lambda.true[j, , ] %*% y.true[i, ]
+		}
+		if(sameSigma){
+			x_data[i,] <- mvrnorm(n = 1, mu = x_mean, Sigma = Sigma.true)
+		}else{
+			x_data[i,] <- mvrnorm(n = 1, mu = x_mean, Sigma = Sigma.true[j,,])
+		}
+	}
+	matplot(t(x_data), type = "l", col = z.true, lty = 1)
+	legend("bottomleft", paste0("cluster ",1:K.true, ": ",as.character(as.numeric(table(z.true)))), col = 1:K.true, lty = 1)
+	results <- vector('list', length = 7)
+	results[[1]] <- x_data
+	results[[2]] <- z.true
+	results[[3]] <- Lambda.true
+	results[[4]] <- mu.true 
+	results[[5]] <- Sigma.true
+	results[[6]] <- y.true
+	results[[7]] <- w.true
+	names(results) <- c("data", "class", "factorLoadings", "means", "variance","factors","weights")
+	return(results)
+}
+
+
+
+
+
 #' @export
 print.fabMix.object <- function(x, ...){
        if( 'fabMix.object' %in% class(x) ){
